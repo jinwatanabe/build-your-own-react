4d6ee8cbe145c114f55dcbaf7d354d80fc12c8a8
jinwatanabe <jin.watanabe.6g@gmail.com>
2025-05-24 18:05:40 +0900

    createElementまで

---
+++ main.js
@@ -0,0 +1,25 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+      children,
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const element = createElement("h1", { title: "foo" }, "Hello");
+ReactDOM.render(element, container);
